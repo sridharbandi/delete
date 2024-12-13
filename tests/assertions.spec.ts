f33c0a298dbcc48f5003cd32ac66758feeb288f3
Sridhar Bandi <sridhar.bandi.ece@gmail.com>
2024-12-13 21:35:05 +0000

    First Commit

---
+++ tests/assertions.spec.ts
@@ -0,0 +1,22 @@
+import { expect } from "chai";
+
+describe('Use case tests', () => {
+
+    it('3 is greater than 4', () => {
+        expect(3).to.be.above(4)
+    })
+
+    it('Array [8, 9, 11, 23] length is 4', () => {
+        let arr = [8, 9, 11, 23]
+        expect(arr).to.have.lengthOf(4)
+    })
+
+    it('Strict equal 101 to ‘101’', () => {
+        expect(101).to.equal('101')
+    })
+
+    it('Array [‘a’, ‘b’, ‘c’] contains ‘c’', () => {
+        let arr = ['a', 'b', 'c']
+        expect(arr).to.include('c')
+    })
+});
